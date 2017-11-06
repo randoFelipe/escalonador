1b15d2da23cb8e9729ee28607765ecc93c067f1b
Felipe Rando <felipe.rando@rivendel.com.br>
2017-11-06 09:36:10 -0200

    passando codigo para golang

--- Processo.py
+++ Processo.py
@@ -16,3 +16,18 @@ class Processo:
         imprime as informacoes relacionadas ao processo
         """
         print("O processo {} tem duracao de {} e tempo de chegada {}".format(self.pid,self.duracao,self.tempoDeChegada))
+
+//  O simulador deve ter como entrada as informações de cada processo como
+//  PID, duração, tempo de chegada, caso tenha operação de I/O quando elas
+//  devem ser executadas (em relação ao seu tempo de execução). O tempo
+//  do quantum também deve ser descrito no início da simulação.
+type Processo struct{
+    pid int64
+    duracao int64
+    tempoDeChegada int64
+    io bool
+
+    func (p *Processo) imprime() string{
+        return "O processo %s tem duracao de %d e tempo de chegada %s",pid,duracao,tempoDeChegada
+    }
+}
---
+++ src/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "Processo"
+
+from Escalonador import Escalonador
+
+proc1 = Processo(1, 9, 10, False)
+proc2 = Processo(2,10,4,False)
+proc3 = Processo(3,5,0, False)
+proc4 = Processo(4,7,1, False)
+proc5 = Processo(5,2,17, False)
+
+listaDeProcessos = [proc1 ,proc2 ,proc3 ,proc4 ,proc5]
+
+def ordenaProcessos (arrayDeProcessos):
+    """
+    Array para ordenar processos pela ordem de chegada
+    """
+    aux = []
+    for processo in arrayDeProcessos:
+        aux.append(processo.tempoDeChegada)
+    aux.sort()
+    return aux
+
+
+print(ordenaProcessos(listaDeProcessos))
