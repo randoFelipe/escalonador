48aba0d72b168765e0b4c92019ffef6693c95ffa
Felipe Rando <felipe.rando@rivendel.com.br>
2017-11-15 23:17:45 -0200

    adicionado funções escalonador, cpuExecutor e reArrange

--- src/main.go
+++ src/main.go
@@ -10,13 +10,48 @@ type Processo struct {
 	Io               bool
 }
 
-func escalonador(listaDeProcessos []Processo, quantum int8) {
-	var processosNaFila []Processo
-	var tempo int8
+func main() {
+	p1 := Processo{"P1", 9, 10, false}
+	p2 := Processo{"P2", 10, 4, false}
+	p3 := Processo{"P3", 5, 0, false}
+	p4 := Processo{"P4", 7, 1, false}
+	p5 := Processo{"P5", 2, 17, false}
+	listaDeProcessos := [5]Processo{p1, p2, p3, p4, p5}
+	quantum := 4
+	fmt.Println("*************\nEscalonador Round-Robin\n*************")
+
+	fmt.Println("*************\nEncerrando simulação de escalonamento\n*************")
 
 }
 
-func main() {
-	fmt.Println("*************\nEscalonador Round-Robin\n*************")
+//função que realiza a logica do escalonador
+func escalonador(listaDeProcessos []Processo) {
+	tempo := 0
 
+}
+
+//para cada tempo de duracao de um quantum a função subtrai 1 de duração de um processo
+func cpuExecutor(p Processo, quantum int) (duracao int) {
+	for i := 1; i <= quantum; i++ {
+		if p.Duracao > 0 {
+			p.Duracao -= 1
+		}
+
+	}
+	return
+}
+
+//função responsavel por realocar os objetos dentro de uma array
+//ela pega o primeiro elemento da array e coloca no fim, realocando todos os outros objetos para
+//sua posição -1
+func reArrange(lista []Processo) (listaReArranjada []Processo) {
+	var aux Processo
+	for i := 0; i < len(lista)-1; i++ {
+		if i == 0 {
+			aux = lista[0]
+		}
+		lista[i] = lista[i+1]
+	}
+	lista[len(lista)-1] = aux
+	return
 }
